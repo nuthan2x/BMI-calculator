69eee98f3be2ed2e8e5e0f586dbbdb80d0284df4
Nuthan Prasad <nuthan1159@gmail.com>
2022-08-12 09:45:44 +0530

    fgh

---
+++ calci.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+    app.use(bodyparser.urlencoded({extended: true}));
+
+    app.post("/bmicalculator",function(req,res){
+
+        var w = Number(req.body.weight);
+        var h = Number(req.body.height);
+
+
+
+        res.send("your BMI is " + (w/h**2).toFixed(2) );
+    });
+    
+    app.get("/",function(req,res){
+        res.sendFile(__dirname + "/bmical.html")
+    });
+
+    app.listen(process.env.PORT ,function(){
+    console.log("server started.");
+});
+
+
